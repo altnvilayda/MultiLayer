8d119194a4ac1e86e126a51e2cfe0c41b7b696f5
altnvilayda <48130952+altnvilayda@users.noreply.github.com>
2021-05-21 17:41:51 +0300

    Create Process.h

---
+++ Process.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+int YPoint(int x, double* w, int = 1);
+
+int sgn(float);
+
+float SigmoidFunc(float net);
+
+int Sum(int* vec, int length);
+
+float Sum(float* vec, int length);
+
